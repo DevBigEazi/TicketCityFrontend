31214cf4c809e862a2c2428c69f6ea881017e485
devbigeazi <adesholatajudeen1@gmail.com>
2025-03-14 10:35:24 +0100

    added pwa

---
+++ src/sw.ts
@@ -0,0 +1,22 @@
+import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
+import { Route, registerRoute } from 'workbox-routing';
+import { CacheFirst } from 'workbox-strategies';
+
+declare let self: ServiceWorkerGlobalScope;
+
+cleanupOutdatedCaches();
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+self.skipWaiting();
+
+// cache images
+const imageRoute = new Route(
+  ({ request, sameOrigin }) => {
+    return sameOrigin && request.destination === 'image';
+  },
+  new CacheFirst({
+    cacheName: 'images',
+  }),
+);
+registerRoute(imageRoute);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,54 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.ts',
+      registerType: 'autoUpdate',
+      injectManifest: {
+        swDest: 'dist/sw.js',
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // Increase to 5 MiB
+      },
+      manifest: {
+        name: 'Ticket City',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        theme_color: '#1a1a1a',
+        background_color: '#1a1a1a',
+        start_url: '/',
+        display: 'standalone',
+        orientation: 'portrait',
+      },
+    }),
+  ],
   base: './',
 });
